dcf4164d286a1ebe2e77daf5fff5560f2596e49e
KCCI119\kcci <rundollyrun8@gmail.com>
2024-04-08 13:30:53 +0900

    feat : member, admin 관련 DTO 생성

---
+++ src/main/java/dao/adminDAO.java
@@ -0,0 +1,23 @@
+package dao;
+
+/**
+ * admin DAO
+ * @author ljy
+ * @since 2024-04-08
+ * **/
+public class adminDAO {
+
+	public adminDAO() {
+		
+	} // adminDAO() END
+
+	/**
+	 * admin Login check
+	 * @param String id, String pw
+	 * @return boolean
+	 * **/
+	public boolean adminLogin(String id, String pw) {
+		
+		return false;
+	} //  adminLogin() END
+}
