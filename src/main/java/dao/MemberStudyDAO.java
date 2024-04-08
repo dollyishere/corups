71ff456a456542b03764ea58de24f3be7c4ff064
KCCI119\kcci <rundollyrun8@gmail.com>
2024-04-08 14:09:17 +0900

    feat : MemberStudy 관련 DAO, DTO 설정

---
+++ src/main/java/dao/MemberStudyDAO.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.util.ArrayList;
+
+/**
+ * member & study DAO
+ * @author ljy
+ * @since 2024-04-08
+ * **/
+public class MemberStudyDAO {
+
+	public MemberStudyDAO() {
+		
+	} // MemberStudyDAO() END
+	
+	/**
+	 * get member's ids from studyNo
+	 * @param int studyNo
+	 * @return ArrayList<String>
+	 * **/
+	public ArrayList<String> memberIdList(int studyNo) {
+		return null;
+	} // memberIdList() END
+
+	/**
+	 * get study numbers list from memberId
+	 * @param String memberId
+	 * @return ArrayList<Integer>
+	 * **/
+	public ArrayList<Integer> studyNoList(String memberId) {
+		return null;
+	} // studyNoList() END
+}
