0e8a251eec2db904d5583b9959d042bc8da2d5a0
NaSoRim <lasolim@naver.com>
2024-04-09 19:37:09 +0900

    remove : staus 관련 DAO, DTO 삭제

--- src/main/java/dao/StatusDAO.java
+++ src/main/java/dao/StatusDAO.java
@@ -1,45 +1,42 @@
-package dao;
-
-import dto.StatusDTO;
-
-/**
- * Status DAO
+/*
+ * package dao;
  * 
- * @author nsr
- * @since 2024-04-08
- **/
-public class StatusDAO {
-
-	public StatusDAO() {
-	}
-
-	/**
+ * import dto.StatusDTO;
+ * 
+ *//**
+	 * Status DAO
+	 * 
+	 * @author nsr
+	 * @since 2024-04-08
+	 **/
+/*
+ * public class StatusDAO {
+ * 
+ * public StatusDAO() { }
+ * 
+ *//**
 	 * Get Status Detail
 	 * 
 	 * @param int no
 	 * @return StatusDTO
 	 */
-	public StatusDTO statusDetail(int no) {
-		return null;
-	}
-
-	/**
+/*
+ * public StatusDTO statusDetail(int no) { return null; }
+ * 
+ *//**
 	 * Insert Status
 	 * 
 	 * @param StatusDTO statusDTO
 	 * @return boolean
 	 */
-	public boolean insertStatus(StatusDTO statusDTO) {
-		return false;
-	}
-
-	/**
+/*
+ * public boolean insertStatus(StatusDTO statusDTO) { return false; }
+ * 
+ *//**
 	 * Update Status
 	 * 
 	 * @param StatusDTO statusDTO
 	 * @return boolean
-	 */
-	public boolean updateStatus(StatusDTO statusDTO) {
-		return false;
-	}
-}
+	 *//*
+		 * public boolean updateStatus(StatusDTO statusDTO) { return false; } }
+		 */
--- src/main/java/dto/StatusDTO.java
+++ src/main/java/dto/StatusDTO.java
@@ -1,37 +1,25 @@
-package dto;
-/**
- * Status DTO
- * 
- * @author nsr
- * @since 2024-04-08
- **/
-public class StatusDTO {
-	private int no;
-	private String status;
-
-	public StatusDTO() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	@Override
-	public String toString() {
-		return "StatusDTO [no=" + no + ", status=" + status + "]";
-	}
-
-}
+/*
+ * package dto;
+ *//**
+	 * Status DTO
+	 * 
+	 * @author nsr
+	 * @since 2024-04-08
+	 **//*
+		 * public class StatusDTO { private int no; private String status;
+		 * 
+		 * public StatusDTO() { // TODO Auto-generated constructor stub }
+		 * 
+		 * public int getNo() { return no; }
+		 * 
+		 * public void setNo(int no) { this.no = no; }
+		 * 
+		 * public String getStatus() { return status; }
+		 * 
+		 * public void setStatus(String status) { this.status = status; }
+		 * 
+		 * @Override public String toString() { return "StatusDTO [no=" + no +
+		 * ", status=" + status + "]"; }
+		 * 
+		 * }
+		 */
