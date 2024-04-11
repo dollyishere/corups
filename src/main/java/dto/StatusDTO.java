cd7a26ff17d7bbcc4a4d245684777f61d50957d1
NaSoRim <lasolim@naver.com>
2024-04-11 16:56:18 +0900

    feat: status DAO,DTO 수정

--- src/main/java/dto/StatusDTO.java
+++ src/main/java/dto/StatusDTO.java
@@ -1,25 +1,47 @@
-/*
- * package dto;
- *//**
-	 * Status DTO
-	 * 
-	 * @author nsr
-	 * @since 2024-04-08
-	 **//*
-		 * public class StatusDTO { private int no; private String status;
-		 * 
-		 * public StatusDTO() { // TODO Auto-generated constructor stub }
-		 * 
-		 * public int getNo() { return no; }
-		 * 
-		 * public void setNo(int no) { this.no = no; }
-		 * 
-		 * public String getStatus() { return status; }
-		 * 
-		 * public void setStatus(String status) { this.status = status; }
-		 * 
-		 * @Override public String toString() { return "StatusDTO [no=" + no +
-		 * ", status=" + status + "]"; }
-		 * 
-		 * }
-		 */
+package dto;
+/**
+ * Status-Todo DTO
+ * 
+ * @author nsr
+ * @since 2024-04-08
+ **/
+public class StatusDTO {
+	private String	memberId;
+	private int	todoNo;
+	private String	status;
+
+	public StatusDTO() {
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public int getTodoNo() {
+		return todoNo;
+	}
+
+	public void setTodoNo(int todoNo) {
+		this.todoNo = todoNo;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "StatusDTO [memberId=" + memberId + ", todoNo=" + todoNo + ", status=" + status + "]";
+	}
+
+
+	
+}
